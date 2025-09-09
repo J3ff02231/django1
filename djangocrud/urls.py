8a2fddcb6b1ec4b745b8af667d9bc87d7673694e
J3ff02231 <jeffostalin47@gmail.com>
2025-09-08 22:21:27 -0500

    djangocrud

--- djangocrud/urls.py
+++ djangocrud/urls.py
@@ -19,6 +19,8 @@ from django.urls import path
 from tasks import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('signup/', views.signup, name='signup'),
+    path("admin/", admin.site.urls),
+    path("", views.home, name="home"),
+    path("signup/", views.signup, name="signup"),
+    path("tasks/", views.tasks, name="tasks"),
 ]
